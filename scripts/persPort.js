d8c6c767f1f3541744e7a62a81ec3830f7c1da92
britten <britten.bell@gmail.com>
2019-03-14 18:57:11 -0600

    Initial commit. Star wars tab added

---
+++ scripts/persPort.js
@@ -0,0 +1,30 @@
+import{ films } from '../data/films.js'
+
+// const result = films.filter(film => film.episode_id)
+// console.log(result)
+films.sort((a,b)=> (a.episode_id> b.episode_id) ? 1 : -1)
+const intro = document.querySelector('.intro')
+
+const result = films.sort(film => film.episode_id)
+
+console.log(result)
+
+films.forEach((film) => {
+
+ let tile = document.createElement('div')
+
+ let titleElement = document.createElement('h1')
+ let crawlElement = document.createElement('h3')
+
+ titleElement.textContent = film.title
+ crawlElement.textContent = film.opening_crawl
+
+//  tile.appendChild(titleElement)
+//  tile.appendChild(crawlElement)
+//  intro.appendChild(tile)
+})
+// let titleElement = document.querySelector(".title")
+// let crawlElement = document.querySelector(".crawl")
+
+// titleElement.textContent = films[0].title
+// crawlElement.textContent = films[0].opening_crawl
---
+++ scripts/swpeople.js
@@ -0,0 +1,180 @@
+import { people } from '../data/people.js'
+import { planets } from '../data/planets.js'
+
+
+
+//character act string funciton MDN
+//Slice string method MDN "how to get a section of a string from an index"
+const getLastNumber = (url) => {
+  let end = url.lastIndexOf('/')
+  console.log(end)
+  let start = end -2
+ if(url.charAt(start) === '/'){
+   start ++
+ }
+return (url.slice(start, end))
+}
+//we are going to import from one array then within that loop we will do search into another array
+const allHomeWorlds = people.map(person => {
+  let foundWorld = planets.find(planet =>{
+    return planet.url === person.homeworld
+  })
+  let imageURL  = getLastNumber(person.url)
+  return {
+    name: person.name, 
+    home: foundWorld.name,
+    gender: person.gender,
+    imagePath: `https://starwars-visualguide.com/assets/img/characters/${imageURL}.jpg`,
+  force: person.force,
+}
+})
+
+console.log(allHomeWorlds)
+
+
+//https://starwars-visualguide.com/assets/img/characters/1.jpg
+//----> now we insert into the DOM
+
+
+//setting the variables of men women and other in order to later use them in functions
+//each variable is assigned a value by reaching into the imported "people" js
+
+const men = allHomeWorlds.filter(person=> person.gender ==='male')
+const women = allHomeWorlds.filter(person => person.gender ==='female')
+const other = allHomeWorlds.filter(person => (person.gender ==='n/a') || (person.gender ==='hermaphrodite') || (person.gender ==='other'))
+// console.log(men, other,women)
+
+const lightSide = allHomeWorlds.filter(person=> person.force ==='light')
+const darkSide = allHomeWorlds.filter(person=> person.force === 'dark')
+
+
+console.log(darkSide)
+
+
+
+
+
+//This is just creating a mainContainer element to hold all of the people information
+
+const mainContainer = document.createElement('div')
+mainContainer.className = 'container'
+document.body.appendChild(mainContainer)
+
+const removeCards = () => {
+  let removeDiv = document.querySelector(".container");
+  while (removeDiv.firstChild) {
+      removeDiv.removeChild(removeDiv.firstChild);
+  }
+}
+
+//trying to get a button to filter the genders
+var females = document.getElementById('females')
+var males = document.getElementById('males')
+
+const createCards = ((personArray) => {
+  removeCards()
+  personArray.forEach((person) => {
+  
+  let personElement = document.createElement('div')
+  let planetElement = document.createElement ('p')
+  let imageElement = document.createElement ('img')
+
+
+  if (person.force ==='light'){
+    personElement.classList.add('box lightSide personBox');
+  } 
+  if (person.force ==='dark'){
+    personElement.className = 'box darkSide personBox'
+  }
+  if (person.force ==='yellow'){
+    personElement.className = 'box yellow personBox'
+  }
+ 
+
+  // personElement.className = 'personBox'
+  personElement.textContent = person.name
+  planetElement.textContent = person.home
+  
+ 
+
+  imageElement.src = person.imagePath
+
+  
+  personElement.appendChild(imageElement)
+  personElement.appendChild(planetElement)
+  mainContainer.appendChild(personElement)
+  
+})
+})
+
+// mouse event for the button to filter out just the males--------------------
+
+males.addEventListener('click', createCards.bind(this, men))
+females.addEventListener('click', createCards.bind(this, women))
+
+// function filterMales(){
+
+//   men.forEach((man) => {
+//   let manElement = document.createElement('div')
+//   let imageElement = document.createElement('img')
+
+// manElement.className = 'personBox'
+// manElement.textContent = man.name
+
+// imageElement.src= man.imagePath
+
+// manElement.appendChild(imageElement)
+// mainContainer.appendChild(manElement)
+// })
+// males.removeEventListener('click', filterMales)
+
+// }
+//trying to add the darkside light side function--------------
+
+
+//event listener to filter out the females---------------------------------------
+// females.addEventListener('click', filterFemales)
+
+// function filterFemales(){ 
+ 
+// women.forEach((woman) => { 
+//   let womanElement = document.createElement('div')
+//   let imageElement = document.createElement('img')
+//   // let planetElement = document.createElement('p')
+
+  
+// womanElement.className = 'box'
+// womanElement.textContent = woman.name
+
+
+// imageElement.src= woman.imagePath
+
+// womanElement.appendChild(imageElement)
+
+// mainContainer.appendChild(womanElement)
+// }) 
+// females.removeEventListener('click',filterFemales, false );
+// if (false){
+//   location.reload()
+// }
+// }
+
+//end of my trials-----------------------------------------------------------------
+
+
+
+// women.forEach((woman)=> {
+//   let womanElement = document.createElement('div')
+  
+// womanElement.className = 'box'
+// womanElement.textContent = woman.name
+// mainContainer.appendChild(womanElement)
+// })
+
+
+
+
+
+
+
+//removeCards()
